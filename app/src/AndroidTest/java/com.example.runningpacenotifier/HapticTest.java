766647e231abb72669eb175d6453e6dedb785c1d
Kiwifan1 <joshua.venable@outlook.com>
2022-10-15 14:43:54 -0700

    Haptic should work

---
+++ app/src/AndroidTest/java/com.example.runningpacenotifier/HapticTest.java
@@ -0,0 +1,34 @@
+/**
+ * Haptic Manager Test Class
+ */
+
+package com.example.runningpacenotifier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ **/
+@RunWith(AndroidJUnit4.class)
+public class HapticTest {
+
+    @Test
+    public void testVibrate() {
+        // test the HapticManager vibrate method
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        HapticManager hapticManager = new HapticManager(context);
+        hapticManager.vibrate();
+    }
+
+    @Test
+    public void testHapticEnabled() {
+        // test the HapticManager hapticEnabled method
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        HapticManager hapticManager = new HapticManager(context);
+        boolean hapticEnabled = hapticManager.hapticEnabled(context);
+        assertTrue(hapticEnabled);
+    }
+}
---
+++ app/src/main/java/com/example/runningpacenotifier/HapticManager.java
@@ -0,0 +1,66 @@
+/**
+ * Name: Joshua Venable
+ * Date: 10/15/2022
+ * Description: Haptic feedback manager for Android wearOS devices
+ * Notes: 
+ * TODO: Implement haptic feedback if a user is running too fast or too slow
+ * 
+ **/
+
+package com.example.runningpacenotifier;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+public class HapticManager {
+    private Context mContext;
+    private Vibrator mVibrator;
+
+    public HapticManager(Context context) {
+        mContext = context;
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    /**
+     * If the user has enabled haptic feedback in the system settings, return true, otherwise return
+     * false.
+     * 
+     * @param context The context of the application.
+     * @return The value of the setting for haptic feedback.
+     */
+    public static boolean hapticEnabled(Context context) {
+        int enabled = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 0);
+        return enabled != 0;
+    }
+
+    /**
+     * If the device has a vibrator, vibrate for 1000 milliseconds with the default amplitude.
+     */
+    public void vibrate() {
+        if (mVibrator.hasVibrator()) {
+            mVibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+        }
+    }
+
+    /**
+     * Vibrate for 0 milliseconds, then vibrate for 100 milliseconds, then vibrate for 100
+     * milliseconds, then vibrate for 100 milliseconds, then vibrate for 100 milliseconds.
+     */
+    public void vibrateSlowDown() {
+        if(mVibrator.hasVibrator()) {
+            mVibrator.vibrate(VibrationEffect.createWaveform(new long[] {0, 100, 100, 100, 100}, 1));
+        }
+    }
+
+    /**
+     * Vibrate for 1 second, pause for 1 second, vibrate for 1 second, pause for 1 second.
+     */
+    public void vibrateSpeedUp() {
+        if(mVibrator.hasVibrator()) {
+            mVibrator.vibrate(VibrationEffect.createWaveform(new long[] {0, 1000, 1000, 1000}, 0));
+        }
+    }
+}
